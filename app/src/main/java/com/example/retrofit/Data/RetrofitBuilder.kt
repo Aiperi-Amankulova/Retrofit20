21e7e60c0a33684f4c5ff7b0ad8c83afa3bf90fd
Aiperi <aip.amankulova@gmail.com>
2020-08-23 16:04:05 +0600

    Add new RetrofitBuilder.kt

---
+++ app/src/main/java/com/example/retrofit/Data/RetrofitBuilder.kt
@@ -0,0 +1,4 @@
+package com.example.retrofit.Data
+
+class RetrofitBuilder {
+}
--- app/src/main/java/com/example/retrofit/Ui/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/Ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.retrofit
+package com.example.retrofit.Ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.retrofit.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
